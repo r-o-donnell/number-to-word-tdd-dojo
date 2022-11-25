5c8c34c84ce884697907d1567bac6c47836f3482
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-11-25 11:26:23 +0000

    refactor

--- main.js
+++ main.js
@@ -1,13 +1,30 @@
 const number_to_string = (int) => {
-  if (typeof int !== "number") {
+  if (isInValidInput(int)) {
     return "Bad Request"
-  } 
-  if(int==1){
-      return "one";
   }
-  if(int==2){
-    return "two";
-}
+  return convertDigitToString(int);
+
 };
+
+const isInValidInput =(int) =>{
+  return typeof int !== "number";
+}
+
+const convertDigitToString = (digit) => {
+  const library = {
+    0: "zero",
+    1: "one",
+    2: "two",
+    // 3: "three",
+    // 4: "four",
+    // 5: "five",
+    // 6: "six",
+    // 7: "seven",
+    // 8: "eight",
+    // 9: "nine",
+  }
+
+  return library[digit];
+}
 
 module.exports = number_to_string;
