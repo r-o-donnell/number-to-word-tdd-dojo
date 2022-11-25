8e8e740f84d6c1f20a61c1eb104213ac40858bb8
Thomas.Mcinally <thomas.mcinally@oaknorth.co.uk>
2022-11-25 11:15:34 +0000

    input validation

--- main.js
+++ main.js
@@ -1,5 +1,13 @@
-const number_to_string = () => {
-  return "number";
+const number_to_string = (int) => {
+  if (typeof int !== "number") {
+    return "Bad Request"
+  } 
+  if(int==1){
+      return "one";
+  }
+  if(int==2){
+    return "two";
+}
 };
 
 module.exports = number_to_string;
--- main.spec.js
+++ main.spec.js
@@ -1,8 +1,27 @@
 const number_to_string = require("./main");
 
 describe("number to string", () => {
-  it("passes", () => {
-    const number = number_to_string();
-    expect(number).toBe("number");
+  it("returns one if pass in 1", () => {
+
+    const number = number_to_string(1);
+    expect(number).toBe("one");
+
   });
+
+  it("returns two if pass in 2", () => {
+    const number = number_to_string(2);
+    expect(number).toBe("two");
+  })
+});
+
+
+describe("input validation", () => {  
+  it("returns bad request for string inputs", () => {
+    const number = number_to_string("#");
+    expect(number).toBe("Bad Request");
+  })
+  it("returns bad request for boolean", () => {
+    const number = number_to_string(false);
+    expect(number).toBe("Bad Request");
+  })
 });
