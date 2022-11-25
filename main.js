755735d74f14d424537efeb1cc2b3fca59a86855
Anitha Gopinath <78701588+AnithaG-Oak@users.noreply.github.com>
2022-11-25 16:29:45 +0530

    Added 0,1,2

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
-const number_to_string = () => {
-  return "number";
+const number_to_string = (number) => {
+  if (number == 1)
+    return "one";
+  else if (number == 2)
+    return "two";
+  return "zero";
 };
 
 module.exports = number_to_string;
--- main.spec.js
+++ main.spec.js
@@ -1,8 +1,18 @@
 const number_to_string = require("./main");
 
 describe("number to string", () => {
-  it("passes", () => {
-    const number = number_to_string();
-    expect(number).toBe("number");
+  it("checks 0 as string zero", () => {
+    const number = number_to_string(0);
+    expect(number).toBe("zero");
+  });
+
+  it("checks 1 as string one", () => {
+    const number = number_to_string(1);
+    expect(number).toBe("one");
+  });
+
+  it("checks 2 as string two", () => {
+    const number = number_to_string(2);
+    expect(number).toBe("two");
   });
 });
