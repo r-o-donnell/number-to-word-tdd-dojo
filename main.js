ee40104ff44c222557e3a7299ffab030f14717e2
Anitha Gopinath <78701588+AnithaG-Oak@users.noreply.github.com>
2022-11-25 17:17:37 +0530

    Added 100 - 199

--- main.js
+++ main.js
@@ -1,7 +1,53 @@
-const number_to_string = (number) => {
-  digit_map = {0: "zero", 1: "one", 2: "two"}
+const digit_map = {
+  0: "zero",
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  10: "ten",
+  11: "eleven",
+  12: "twelve",
+  13: "thirteen",
+  14: "fourteen",
+  15: "fifteen",
+  16: "sixteen",
+  17: "seventeen",
+  18: "eighteen",
+  19: "nineteen",
+  20: "twenty",
+  30: "thirty",
+  40: "forty",
+  50: "fifty",
+  60: "sixty",
+  70: "seventy",
+  80: "eighty",
+  90: "ninty",
+  100: "hundred"
+}
 
-  return digit_map[number];
+const two_digit_number_to_string = (number) => {
+  if (number in digit_map) {
+    return digit_map[number];
+  }
+  tens_place = Math.floor(number/10)
+  ones_place = number%10
+  return digit_map[tens_place*10]+"-"+digit_map[ones_place];
+}
+
+const number_to_string = (number) => {
+  if (number in digit_map) {
+    return digit_map[number];
+  }
+  if (number <= 100 ) {
+    return two_digit_number_to_string(number)
+  }
+  hundreds_place = Math.floor(number/100)
+  return digit_map[hundreds_place * 100]+"-"+two_digit_number_to_string(number%100)
 };
 
 module.exports = number_to_string;
--- main.spec.js
+++ main.spec.js
@@ -16,4 +16,84 @@ describe("number to string", () => {
     expect(number).toBe("two");
   });
 
+  it("checks 5 as string five", () => {
+    const number = number_to_string(5);
+    expect(number).toBe("five");
+  });
+
+  it("checks 8 as string eight", () => {
+    const number = number_to_string(8);
+    expect(number).toBe("eight");
+  });
+
+  it("checks 10 as string ten", () => {
+    const number = number_to_string(10);
+    expect(number).toBe("ten");
+  });
+
+  it("checks 20 as string twenty", () => {
+    const number = number_to_string(20);
+    expect(number).toBe("twenty");
+  });
+
+  it("checks 21 as string twenty-one", () => {
+    const number = number_to_string(21);
+    expect(number).toBe("twenty-one");
+  });
+
+  it("checks 29 as string twenty-nine", () => {
+    const number = number_to_string(29);
+    expect(number).toBe("twenty-nine");
+  });
+
+  it("checks 30 as string thirty", () => {
+    const number = number_to_string(30);
+    expect(number).toBe("thirty");
+  });
+
+  it("checks 43 as string forty-three", () => {
+    const number = number_to_string(43);
+    expect(number).toBe("forty-three");
+  });
+
+  it("checks 54 as string fifty-four", () => {
+    const number = number_to_string(54);
+    expect(number).toBe("fifty-four");
+  });
+
+  it("checks 65 as string sixty-five", () => {
+    const number = number_to_string(65);
+    expect(number).toBe("sixty-five");
+  });
+
+  it("checks 76 as string seventy-six", () => {
+    const number = number_to_string(76);
+    expect(number).toBe("seventy-six");
+  });
+
+  it("checks 88 as string eighty-eight", () => {
+    const number = number_to_string(88);
+    expect(number).toBe("eighty-eight");
+  });
+
+  it("checks 99 as string ninty-nine", () => {
+    const number = number_to_string(99);
+    expect(number).toBe("ninty-nine");
+  });
+
+  it("checks 100 as string hundred", () => {
+    const number = number_to_string(100);
+    expect(number).toBe("hundred");
+  });
+
+  it("checks 111 as string hundred-eleven", () => {
+    const number = number_to_string(111);
+    expect(number).toBe("hundred-eleven");
+  });
+
+  it("checks 188 as string hundred-eighty-eight", () => {
+    const number = number_to_string(188);
+    expect(number).toBe("hundred-eighty-eight");
+  });
+
 });
